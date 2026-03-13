07d4b779e56e46e3a2c7b0439eedc8ceb16ce575
Dhirendra Singh <dhirendrasinghaws@gmail.com>
2026-03-14 00:00:45 +0530

    Added skill demand report and visualization

--- Job-market-analyzer/src/load_data.py
+++ Job-market-analyzer/src/load_data.py
@@ -11,3 +11,11 @@ print(df.info())
 
 print("\nMost demanded skills:\n")
 print(df.loc[df["skill_abr"].value_counts().head(10)])
+
+top_skills = df["skill_abr"].value_counts().head(10)
+
+print("\nTop Skills:\n")
+print(top_skills)
+
+# save report
+top_skills.to_csv("C:/Users/thera/OneDrive/Desktop/python-data-projects/Job-market-analyzer/output/top_skills_report.csv")
